ef67ad6a6292d5403a974218341b2bdabd3dce4c
polarisyb <polaris@acrosspace.com>
2023-05-17 10:46:15 +0900

    주석 추가 및 crazyButton 좌표 값 변경(테스트)

--- index.js
+++ index.js
@@ -13,20 +13,31 @@ app.get('/', (req, res) => {
 
 app.use(express.static('public'));
 
+// 웹 소켓 연결 시
 io.on('connection', function(socket){
    console.log('A user connected');
 
+   // 연결 종료 시 - 새로고침시 disconnected 문구와 A use connected 문구가 차례로 출력
    socket.on('disconnect', () => {
       console.log('disconnected');
    });
+   
+   // app.js 의 startGame 이벤트 수신 시 - startGame 버튼 클릭 시 동작 
    socket.on('startGame', () => {
       console.log('Game started');
       io.sockets.emit('게임을 시작합니다');
    });
+
+   // app.js 의 home 이벤트 수신 시 - home 버튼 클릭 시 동작
+   // 그리고 이를 송신자를 제외한 모든 사용자에게 전달한다.
    socket.on('home', () => {
       console.log('home button pressed');
       io.emit('home');
    });
+
+   // app.js 의 crazyIsClicked 이벤트 수신 시 - 개체 클릭 시 동작
+   // data 인자로 개체의 랜덤 좌표를 받는다.
+   // 그리고 이를 송신자를 제외한 서버에 접속해 있는 모든 사용자에게 전달한다.
    socket.on('crazyIsClicked', data => {
       io.emit('crazyIsClicked', data);
    });
--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,25 @@
-
-
-  
-
-
 const socket = io();
 
-const startingSection = document.querySelector('.starting-section');
 const crazyButton = document.getElementById('crazyButton');
 const startButton = document.getElementById('startButton');
+const startingSection = document.querySelector('.starting-section');
 const homeBtn = document.querySelector('.home-btn');
 
+// Start 버튼을 숨기는 함수
 const hideStartButton = () => {
   startButton.style.display = 'none';
   crazyButton.style.display = 'block';
   startingSection.style.display = 'none';
 };
 
+// Start 버튼을 보이게 하는 함수
 const showStartButton = () => {
   startButton.style.display = 'block';
   crazyButton.style.display = 'none';
   startingSection.style.display = 'block';
 };
 
+// crazyButton 개체의 top, left, animation 스타일을 담은 함수 
 const goCrazy = (offLeft, offTop) => {
   let top, left;
 
@@ -33,23 +31,31 @@ const goCrazy = (offLeft, offTop) => {
   crazyButton.style.animation = "none";
 };
 
+// startButton 클릭시 startGame 이벤트를 전달하고  hideStartButton 함수를 작동
 startButton.addEventListener('click' , () => {
+  // 송신자에게만 이벤트를 전달
   socket.emit('startGame');
   hideStartButton();
 });
 
+// homeBtn 클릭시 home 이벤트를 전달하고 showStartButton 함수를 작동
 homeBtn.addEventListener('click', () => {
+  // 송신자에게만 이벤트를 전달
   socket.emit('home');
   showStartButton();
 });
 
+// crazyButton 클릭시 crazyIsClicked 이벤트를 전달하고 data로 offsetLeft, offsetTop 을 전달
 crazyButton.addEventListener('click', () => {
+  // 송신자에게만 이벤트를 전달
   socket.emit('crazyIsClicked', {
-    offsetLeft: Math.random() * ((window.innerWidth - crazyButton.clientWidth) - 100),
-    offsetTop: Math.random() * ((window.innerHeight - crazyButton.clientHeight) - 50)
+    offsetLeft: Math.random() * ((window.innerWidth - crazyButton.clientWidth) - 200),
+    offsetTop: Math.random() * ((window.innerHeight - crazyButton.clientHeight) - 100)
   });
 });
 
+// index.js 에서 crazyIsClicked 이벤트를 전달받고 data로 전달받은 offsetLeft, offsetTop 값으로 개체의 x축 좌표 값과 y축 좌표 값을 설정
 socket.on('crazyIsClicked', data => {
   goCrazy(data.offsetLeft, data.offsetTop);
+  console.log(data);
 });

b3c3c900b3716af66a08f57d1006fb28370135c9
polarisyb <polaris@acrosspace.com>
2023-05-18 22:42:39 +0900

    chat.js - 클라이언트

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,20 +1,48 @@
-const socket = io();
+document.addEventListener("DOMContentLoaded", () => {
+  // 소켓 서버에 연결한다
+  let socket = io();
 
-let messages = document.getElementById('messages');
-let form = document.getElementById('form');
-let input = document.getElementById('input');
-
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  if (input.value) {
-    socket.emit('chat message', input.value);
-    input.value = '';
+  // 알파벳 소문자로 이루어진 세 글자의 임의의 이름을 생성하여 반환하는 함수
+  const makeRandomName = () => {
+    let name = "";
+    let possible = "abcdefghijklmnopqrstuvwxyz";
+    for (let i = 0; i < 3; i++) {
+      name += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return name;
   };
-});
 
-socket.on('chat message', msg => {
-  let item = document.createElement('li');
-  item.innerHTML = msg;
-  messages.appendChild(item);
-  window.scrollTo(0, document.body.scrollHeight);
+  // 서버로 자신의 정보를 전송한다.
+  socket.emit("login", {
+    name: makeRandomName(),
+    userid: "polaris@acrosspace.com"
+  });
+
+  // 서버로부터의 메시지가 수신되면
+  socket.on("login", data => {
+    let chatLogs = document.getElementById("chatLogs");
+    let newMessage = document.createElement("div");
+    newMessage.innerHTML = "<strong>" + data + "</strong> has joined";
+    chatLogs.appendChild(newMessage);
+  });
+
+  // 서버로부터의 메시지가 수신되면
+  socket.on("chat", data => {
+    let chatLogs = document.getElementById("chatLogs");
+    let newMessage = document.createElement("div");
+    newMessage.innerHTML = data.msg + " : from <strong>" + data.from.name + "</strong>";
+    chatLogs.appendChild(newMessage);
+  });
+
+  // 폼 제출 이벤트 핸들러
+  let form = document.querySelector("form");
+  form.addEventListener("submit", e => {
+    e.preventDefault();
+    let msgForm = document.getElementById("msgForm");
+
+    // 서버로 메시지를 전송한다.
+    socket.emit("chat", { msg: msgForm.value });
+    msgForm.value = "";
+  });
+  
 });

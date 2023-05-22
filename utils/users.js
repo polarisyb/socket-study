7222b2db6117cce0ddc17197c2990910cc3e9ef9
polarisyb <polaris@acrosspace.com>
2023-05-22 16:06:05 +0900

    users 주석 수정

--- utils/users.js
+++ utils/users.js
@@ -8,22 +8,23 @@
 const users = [];
 
 // id, username, room 매개변수를 사용하여 새로운 사용자 객체를 생성한다.
-// 이 사용자 객체는 users 라는 배열에 추가되고 함수는 새로 만든 사용자 객체를 반환한다.
+// 이 사용자 객체는 'users' 라는 배열에 추가되고 함수는 새로 만든 사용자 객체를 반환한다.
 const userJoin = (id, username, room) => {
     const user = { id, username, room };
     users.push(user);
+    // console.log(user.id);
 
     return user;
 };
 
-// users 배열에서 id가 일치하는 사용자를 검색
-// 사용자가 발견되면 반환, 그렇지 않으면 undefined 반환
+// 'users' 배열에서 id가 일치하는 사용자를 'users' 배열에서 찾아서 해당 사용자를 반환
+// 일치하는 사용자가 없으면 'undefined'를 반환
 const getCurrentUser = id => {
     return users.find(user => user.id === id);
 };
 
-// users 배열에서 id가 일치하는 사용자를 검색,
-// 사용자가 발견되면 배열에서 제거되고 반환, 그렇지 않으면 undefined 반환
+// 'users' 배열에서 id가 일치하는 사용자를 'users' 배열에서 찾아서 제거하고, 해당 사용자를 반환
+// 일치하는 사용자가 없으면 'undefined' 를 반환
 const userLeave = id => {
     const index = users.findIndex(user => user.id === id);
 
@@ -32,8 +33,12 @@ const userLeave = id => {
     };
 };
 
-// 지정된 방에 있는 사용자 배열을 반환, filter() 메서드를 사용하여 room 속성에 따라 배열을 필터링하여 반환
+// 'users' 배열을 순회하면서 각 사용자의 'room' 값과 인자로 전달된 'room 값이 일치하는 지 확인하고 배열로 반환
 const getRoomUsers = room => {
+    // filter() 'user'를 매개변수로 받고 해당 'user'의 'room' 값과 전달된 'room' 값이 일치하는 지 확인하여
+    // 'true' or 'false'를 반환. 
+    // 'true'를 반환하는 경우 해당 'user' 는 결과 배열에 포함되고,
+    // 'flase' 를 반환하는 경우 해당 'user' 는 결과 배열에서 제외된다.
     return users.filter(user => user.room === room);
 };
 

d12ee64f60e7220c4760a0e9e43cb6fcb9b2a0a8
polarisyb <polaris@acrosspace.com>
2023-05-23 16:27:50 +0900

    코드 개선사항 주석 추가 및 클라이언트 사이드에서 메세지 콘솔 출력

--- client/src/App.js
+++ client/src/App.js
@@ -14,28 +14,37 @@ const App = () => {
   /* 부수 효과 - 컴포넌트 외부의 상태를 변경하거나 네트워크 요청을 보내는 등의 작업 */
   // useEffect는 컴포넌트가 렌더링 될 때마다 실행되며, 의존성 배열을 통해 특정 상태나 프로퍼티가 변경될 때에만 실행되도록 설정할 수 있다.
 
+  /* 이전 코드 개선 사항 */
+  // onConnect, onDisconnect, onFooEvent, sendMessage 함수들을 useEffect 바깥에 정의
+  // 매 렌더링마다 새로운 함수 인스턴스를 생성하는 것을 방지
+  const onConnect = () => {
+    setIsConnected(true);
+  }
+
+  const onDisconnect = () => {
+    setIsConnected(false);
+  }
+
+  const onFooEvent = value => {
+    setFooEvents(previous => [...previous, value]);
+  }
+
+  const sendMessage = value => {
+    console.log(value);
+  };
+
   useEffect(() => {
-    const onConnect = () => {
-      setIsConnected(true);
-    }
-
-    const onDisconnect = () => {
-      setIsConnected(false);
-    }
-
-    const onFooEvent = value => {
-      setFooEvents(previous => [...previous, value]);
-    }
-
     socket.on('connect', onConnect);
     socket.on('disconnect', onDisconnect);
     socket.on('foo', onFooEvent);
+    socket.on('sendMessage', sendMessage);
 
     return () => {
       // socket.off 이벤트 리스너 해제
       socket.off('connect', onConnect);
       socket.off('disconnect', onDisconnect);
       socket.off('foo', onFooEvent);
+      socket.off('sendMessage', sendMessage);
     };
     // 의존성 배열이 비어있다면 ([]), useEffect의 첫번째 인자로 전달된 함수는 컴포넌트가 처음 마운트 될 때만 실행된다.
   }, []);

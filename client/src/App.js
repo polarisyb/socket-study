7535311761a2706fd88d69f2868fcd41083b3812
polarisyb <polaris@acrosspace.com>
2023-05-23 11:00:55 +0900

    client - 간단한 룸과 메시지 소켓 통신

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,43 @@
 import './App.css';
+import io from 'socket.io-client';
+import { useEffect, useState } from 'react';
+
+const socket = io.connect('http://localhost:8000');
 
 const App = () => {
+  const [room, setRoom] = useState('');
+
+  const [message, setMessage] = useState('');
+  const [messageReceived, setMessageReceived] = useState('');
+  
+  const joinRoom = () => {
+    if (room !== '') {
+      socket.emit('joinRoom', room);
+    };
+  };
+  
+  const sendMessage = () => {
+    socket.emit('sendMessage', { message, room });
+  };
+
+  useEffect(() => {
+    socket.on('receiveMessage', data => {
+      setMessageReceived(data.message);
+    });
+  }, []);
+
   return (
     <div className='App'>
-      <input placeholder='Message..'/>
-      <button>Send Message</button>
+      <input placeholder='Room Number' onChange={ e => {
+        setRoom(e.target.value);
+      }} />
+      <button onClick={joinRoom}>Join Room</button>
+      <input placeholder='Message..' onChange={ e => {
+        setMessage(e.target.value);
+      }} />
+      <button onClick={sendMessage}>Send Message</button>
+      <h1>Message:</h1>
+      {messageReceived}
     </div>
   );
 };

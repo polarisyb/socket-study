a2142833f18fe16d3e53e876a8f7f563c0b566dc
polarisyb <polaris@acrosspace.com>
2023-05-24 11:45:00 +0900

    mern-socket app.js 초기 설정

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { socket } from './socket';
 
-
 const App = () => {
 
   return (
     <div className="App">
-      
     </div>
   );
 };

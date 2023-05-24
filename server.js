8759e4bbc616c24455635e94f4af5fcf4c47aa67
polarisyb <polaris@acrosspace.com>
2023-05-25 00:35:03 +0900

    Create, Update 작업 수행(브라우저 출력) 및 주석처리

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ const run = async () => {
     const db = client.db("test");
     const coll = db.collection("test");
     
+    /* Create ( 중복 요소 제외, 중복 되지 않은 요소는 추가, 삽입된 문서들의 _id 값 출력) */
     // const docs = [
     //   {name: "Halley's Comet", officialName: "1P/Halley", orbitalPeriod: 75, radius: 3.4175, mass: 2.2e14},
     //   {name: "Wild2", officialName: "81P/Wild", orbitalPeriod: 6.41, radius: 1.5534, mass: 2.3e13},
@@ -57,8 +58,34 @@ const run = async () => {
     // // 이를 통해 어떤 문서들이 성공적으로 삽입되었는지 확인 가능
     // console.log(result.insertedIds);
     
+    /* Update */
+
+    // filter 객체는 업데이트할 문서를 선택하는데 사용되는 조건을 지정한다.
+    // 빈 객체 '{}'를 사용하여 모든 문서를 선택하고 있다.
+    // 만약 특정 조건에 해당하는 문서만 업데이트 하려면 'filter' 객체에 해당 조건을 지정해야 한다.
+    // ex) filter = { name: 'Comet Hyakutake' } 와 같이 이름이 'Comet Hyakutake'인 문서만 선택하도록 지정할 수 있다.
+    const filter = {};
+    
+    // updateDoc 객체는 업데이트할 내용을 지정한다.
+    // $mul 연산자를 사용하여 'radius' 필드를 1.0204 를 곱셈하는 연산을 수행하고 있다.
+    // 이렇게 지정한 업데이트 연산은 선택된 모든 문서에 대해 적용된다.
+    const updateDoc = {
+      $mul: {
+        radius: 1.0204,
+      },
+    };
+
+    // updateMany 메서드는 업데이트 된 문서의 수를 나타내는 'result.modifiedCount' 를 반환한다.
+    const result = await coll.updateMany(filter, updateDoc);
+    console.log("Number of documents updated: " + result.modifiedCount);
+
+    // coll.find() 는 데이터베이스 컬렉션에서 모든 문서를 검색하는 쿼리를 생성한다.
+    // 이 쿼리를 실행하면 결과로 'cursor' 라는 커서 객체가 반환된다.
     const cursor = coll.find();
+    
+    // toArray 메서드를 호출하여 커서의 모든 문서를 배열로 변환하여 data 라는 변수에 할당한다.
     const data = await cursor.toArray();
+    console.log(data);
     
     io.on('connection', socket => {
       socket.emit('document', data);

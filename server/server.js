e5439269c8044078e6added212b14a8ad8444d0d
polarisyb <polaris@acrosspace.com>
2023-05-24 09:36:34 +0900

    require 문 코드 상단 그룹화, 더 명확한 변수명으로 수정

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const app = express();
 const http = require('http');
 const cors = require('cors');
-const server = http.createServer(app);
+const app = express();
+const httpServer = http.createServer(app);
 const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 
-const io = require('socket.io')(server, {
+const io = require('socket.io')(httpServer, {
   cors: {
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST'],
@@ -42,7 +42,7 @@ io.on('connection', socket => {
   });
 });
 
-server.listen(PORT, err => {
+httpServer.listen(PORT, err => {
   if (err) throw err;
   console.log(`Server running on port ${PORT}`);
 });

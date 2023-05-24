f7c7044821827206df49084f8be48f37a4bf641f
polarisyb <polaris@acrosspace.com>
2023-05-24 09:30:11 +0900

    useSocketEvent 함수 생성과 useCallback을 사용한 코드 개선

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { socket } from './socket';
 import MyForm from './components/MyForm';
 import Events from './components/Events';
@@ -6,6 +6,15 @@ import ConnectionManager from './components/ConnectionManager';
 import ConnectionState from './components/ConnectionState';
 import JoinRoom from './components/JoinRoom';
 
+const useSocketEvent = (event, handler) => {
+  useEffect(() => {
+    socket.on(event, handler);
+    
+    return () => {
+      socket.off(event, handler);
+    };
+  }, [event, handler]);
+};
 
 const App = () => {
   const [isConnected, setIsConnected] = useState(socket.connected);
@@ -18,50 +27,57 @@ const App = () => {
   /* 이전 코드 개선 사항 */
   // onConnect, onDisconnect, onFooEvent, sendMessage 함수들을 useEffect 바깥에 정의
   // 매 렌더링마다 새로운 함수 인스턴스를 생성하는 것을 방지
-  const onConnect = () => {
+  const onConnect = useCallback(() => {
     setIsConnected(true);
-  }
-
-  const onDisconnect = () => {
-    setIsConnected(false);
-  }
-
-  const onFooEvent = value => {
-    setFooEvents(previous => [...previous, value]);
-  }
-
-  const sendMessage = value => {
-    console.log(value);
-  };
-
-  const message = msg => {
-    console.log(msg);
-  };
-
-  const joinMessage = msg => {
-    console.log(msg);
-  };
-
-  useEffect(() => {
-    socket.on('connect', onConnect);
-    socket.on('disconnect', onDisconnect);
-    socket.on('foo', onFooEvent);
-    socket.on('sendMessage', sendMessage);
-    socket.on('message', message);
-    socket.on('joinMessage', joinMessage);
-
-    return () => {
-      // socket.off 이벤트 리스너 해제
-      socket.off('connect', onConnect);
-      socket.off('disconnect', onDisconnect);
-      socket.off('foo', onFooEvent);
-      socket.off('sendMessage', sendMessage);
-      socket.off('message', message);
-      socket.off('joinMessage', joinMessage);
-
-    };
-    // 의존성 배열이 비어있다면 ([]), useEffect의 첫번째 인자로 전달된 함수는 컴포넌트가 처음 마운트 될 때만 실행된다.
   }, []);
+
+  const onDisconnect = useCallback(() => {
+    setIsConnected(false);
+  }, []);
+
+  const onFooEvent = useCallback(value => {
+    setFooEvents(previous => [...previous, value]);
+  }, []);
+  
+  const sendMessage = useCallback(value => {
+    console.log(value);
+  }, []);
+
+  const message = useCallback(msg => {
+    console.log(msg);
+  }, []);
+
+  const joinMessage = useCallback(msg => {
+    console.log(msg);
+  }, []);
+
+  useSocketEvent('connect', onConnect);
+  useSocketEvent('disconnect', onDisconnect);
+  useSocketEvent('foo', onFooEvent);
+  useSocketEvent('sendMessage', sendMessage);
+  useSocketEvent('message', message);
+  useSocketEvent('joinMessage', joinMessage);
+
+  // useEffect(() => {
+  //   socket.on('connect', onConnect);
+  //   socket.on('disconnect', onDisconnect);
+  //   socket.on('foo', onFooEvent);
+  //   socket.on('sendMessage', sendMessage);
+  //   socket.on('message', message);
+  //   socket.on('joinMessage', joinMessage);
+
+  //   return () => {
+  //     // socket.off 이벤트 리스너 해제
+  //     socket.off('connect', onConnect);
+  //     socket.off('disconnect', onDisconnect);
+  //     socket.off('foo', onFooEvent);
+  //     socket.off('sendMessage', sendMessage);
+  //     socket.off('message', message);
+  //     socket.off('joinMessage', joinMessage);
+
+  //   };
+  //   // 의존성 배열이 비어있다면 ([]), useEffect의 첫번째 인자로 전달된 함수는 컴포넌트가 처음 마운트 될 때만 실행된다.
+  // }, []);
 
   return (
     <div className="App">

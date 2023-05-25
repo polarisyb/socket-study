b4c7168a66f5c0e48db5daa27c9f25eeccc5dc4a
polarisyb <polaris@acrosspace.com>
2023-05-25 18:12:44 +0900

    cursor 활용으로 조건을 충족하는 데이터를 출력

--- server.js
+++ server.js
@@ -29,14 +29,14 @@ const client = new MongoClient(uri, {
   }
 });
 
-//// 단일 문서를 컬렉션에 삽입한다. 한 번에 하나의 문서만 삽입할 수 있다.
+// 단일 문서를 컬렉션에 삽입한다. 한 번에 하나의 문서만 삽입할 수 있다.
 // const createListing = async (client, newListing) => {
 //   const result = await client.db('sample_airbnb').collection('listingAndReviews').insertOne(newListing);
 
 //   console.log(`new listing created with the following id: ${result.insertedId}`);
 // };
 
-// // 여러 개의 문서를 컬렉션에 삽입한다. 여러 개의 문서를 배열 형태로 전달하여 여러 개의 문서를 삽입할 수 있다.
+// 여러 개의 문서를 컬렉션에 삽입한다. 여러 개의 문서를 배열 형태로 전달하여 여러 개의 문서를 삽입할 수 있다.
 // const createMultipleListing = async (client, newListings) => {
 //   const result = await client.db('sample_airbnb').collection('listingAndReviews').insertMany(newListings);
 
@@ -64,15 +64,49 @@ const listDatabases = async client => {
 
 // findOneListingByName() - findOne() 메서드를 호출하여 'name' 필드가 'nameOfListing'과 첫 번째로 일치하는 문서를 검색하여 출력하는 함수
 // 주로 단일 문서를 검색하고 반환하는데 쓰인다.
+// const findOneListingByName = async (client, nameOfListing) => {
+//   const result = await client.db('sample_airbnb').collection('listingAndReviews').findOne( {name:nameOfListing} );
 
-const findOneListingByName = async (client, nameOfListing) => {
-  const result = await client.db('sample_airbnb').collection('listingAndReviews').findOne( {name:nameOfListing} );
+//   if (result) {
+//     console.log(`Found a listing in the collection with the name '${nameOfListing}'`);
+//     console.log(result);
+//   } else {
+//     console.log(`No listings found with the name '${nameOfListing}'`);
+//   };
+// };
 
-  if (result) {
-    console.log(`Found a listing in the collection with the name '${nameOfListing}'`);
-    console.log(result);
+// 최소 침실 수와 욕실 수, 가장 최근 리뷰를 기준으로 최대 결과 수를 포함하여 검색 결과를 가져온다.
+/* 'cursor' - MongoDB에서 쿼리 결과를 반복적으로 탐색하고 처리하는데 사용되는 객체
+            - 데이터를 한 번에 모두 가져오지 않고, 필요한 만큼 데이터를 가져올 수 있다.
+            - 결과 집합에서 현재 위치를 나타내는 역할
+*/
+/*
+  toArray() : 'cursor' 의 모든 문서를 배열로 변환한다. 메모리에 모든 문서를 로드하므로, 소량의 결과 집합에 적합하다.
+  forEach() : 'cursor' 의 각 문서에 대해 지정된 함수를 실행한다.
+  next() : 'cursor' 가 가리키고 있는 문서의 다음 문서를 반환한다.
+            처음에 커서를 생성하고 find() 메서드 등을 통해 결과를 가져온 경우, 커서는 첫 번째 문서를 가리킨다.
+            그 후 next()를 호출할 때 마다 커서는 다음 문서로 이동한다.
+  limit() : 반환할 문서의 수를 제한한다.
+  sort() : 문서의 정렬 순서를 지정한다.
+  skip() : 결과에서 일부 문서를 건너뛴다.
+*/
+const findListingsByCriteria = async (client, { minBedrooms = 0, minBathrooms = 0, maxResults = Number.MAX_SAFE_INTEGER} ) => {
+  const cursor = client.db('sample_airbnb').collection('listingAndReviews').find({
+    bedrooms: { $gte: minBedrooms },
+    bathrooms: { $gte: minBathrooms },
+    // sort() 메서드를 사용하여 검색 결과를 가장 최근 리뷰를 기준으로 내림차순으로 정렬 
+  }).sort({ last_review: -1 })
+
+  // limit() 메서드를 사용하여 최대 결과 수 제한
+  .limit(maxResults);
+
+  const results = await cursor.toArray();
+
+  if (results.length > 0) {
+    console.log(`Found listing(s) with at least ${minBedrooms} bedrooms and ${minBathrooms} bathrooms: `);
+    console.log(results);
   } else {
-    console.log(`No listings found with the name '${nameOfListing}'`);
+    console.log(`No listings found with at least ${minBedrooms} bedrooms and ${minBathrooms} bathrooms.`);
   };
 };
 
@@ -132,8 +166,13 @@ const run = async () => {
 
     await listDatabases(client);
 
-    await findOneListingByName(client, 'Infinite Views');
+    // await findOneListingByName(client, 'Infinite Views');
 
+    await findListingsByCriteria(client, {
+      minBedrooms: 4,
+      minBathrooms: 2,
+      maxResults: 5
+    });
 
   } catch (err) {
     console.error('Error:', err);

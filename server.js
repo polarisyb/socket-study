3aa0e805c25f1484fa34989ede395aa855c70f9b
polarisyb <polaris@acrosspace.com>
2023-05-26 16:46:43 +0900

    URI 유효성 검사, 클라이언트에서 보내는 데이터(username, room, message)와 socketId, time을 db에 저장

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const io = require('socket.io')(server);
 // const { Server } = require('socket.io');
 // const io = new Server(server);
 const PORT = process.env.PORT || 8000;
+require('dotenv').config();
 
 /* message, user 모듈 */
 const formatMessage = require('./utils/messages');
@@ -17,10 +18,15 @@ const {
   getRoomUsers
 } = require('./utils/users');
 
+
 /* db 모듈 */
+
+// uri 유효성 검사
+const validateMongoDBUri = require('./db/UriVaildator');
 
 // 새로운 db와 컬렉션 생성
 // const createCollection = require('./db/createCollection');
+const listDatabases = require('./db/listDatabases');
 
 // ChatApp 시스템 이름을 변수에 할당 (단순하게 시스템 메세지 보내는 용도)
 const botName = 'ChatApp bot';
@@ -30,7 +36,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 /* mongoDB 연결 */
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://polaris:gpffov8d@crud.sogjbgj.mongodb.net/";
+const uri = process.env.MONGODB_URI;
+validateMongoDBUri(uri); // MongoDB URI 유효성 검사
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -45,56 +52,49 @@ const client = new MongoClient(uri, {
   }
 });
 
-const listDatabases = async client => {
-  // db() MongoClient 객체에서 현재 연결된 데이터베이스에 대한 레퍼런스를 가져오는 메서드
-  // MongoDB 데이터베이스와 상호작용하는 데 사용된다.
-  
-  // admin() 데이터베이스 객체('db')에서 관리자 관련 작업을 수행하기 위한 메서드
-
-  // listDatabases() 데이터베이스 관리자 객체('admin') 에서 데이터베이스 목록을 가져오는 메서드
-  // 현재 MongoDB 서버에 있는 모든 데이터베이스의 목록을 가져올 수 있다.
-  // 배열 형태로 실제 데이터베이스 목록을 담고 있다.
-  const databasesList = await client.db().admin().listDatabases();
-
-  console.log('Databases: ');
-  databasesList.databases.forEach(db => {
-    console.log(`- ${db.name}`);
-  });
-};
-
-const run = async () => {
+const run = async (socket, client) => {
   try {
 
     await client.connect();
 
-    await client.db('admin').command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-
     const db = client.db('Chat-app');
-    const coll = db.collection('user');
+    const coll = db.collection('message');
 
-    // await createCollection(db, collectionName);
+    socket.on('chatMessage', async msg => {
+      const user = getCurrentUser(socket.id);
 
-    await coll.createIndex({ name: 1 }, { unique: true });
-    const options = { ordered: false };
+      const message = {
+        username: user.username,
+        socketId: socket.id,
+        room: user.room,
+        message: msg,
+        time: socket.handshake.time
+      };
+
+      try {
+        await coll.insertOne(message);
+        console.log('Message inserted into MongoDB');
+
+        io.to(user.room).emit('message', formatMessage(user.username, msg));
+
+      } catch (err) {
+        console.error('Error inserting message:', err);
+      }
+    });
 
     await listDatabases(client);
 
   } catch (err) {
     console.error('Error:', err);
-  } finally {
-    
-    await client.close();
-    console.log("Disconnected from MongoDB");
-    
   }
-}
-run().catch(console.dir);
+};
 
 // connection 이 수립되면 event handler function의 인자로 socket이 들어온다.
 io.on('connection', socket => {
   // socket 에서 송수신되는 모든 이벤트에 대한 리스너를 설정한다.
   // 이벤트 이름을 명시적으로 지정하지 않고 모든 이벤트를 수신할 수 있는 특별한 메서드
+  run(socket, client).catch(console.dir);
+
   socket.onAny( e => {
     console.log(`Socket Event: ${e}`);
   });
@@ -121,21 +121,17 @@ io.on('connection', socket => {
       users: getRoomUsers(user.room)
     });
   });
-
-  // chatMessage 이벤트 수신
-  socket.on('chatMessage', msg => {
-    const user = getCurrentUser(socket.id);
-
-    io.to(user.room).emit('message', formatMessage(user.username, msg));
-  });
   
   // 클라이언트가 연결이 끊어졌을 때 실행
   // 소켓 연결이 끊긴 사용자를 식별하고, 해당 사용자가 속한 채팅방에 메시지를 전송, 사용자 목록 업데이트
   socket.on('disconnect', () => {
+
     const user = userLeave(socket.id);
 
     if (user) {
-      io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`));
+      io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`), () => {
+        console.log(`${user.username} has left the ${user.room}`);
+      });
     
       io.to(user.room).emit('roomUsers', {
         room: user.room,

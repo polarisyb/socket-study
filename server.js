a5ea81b301b7daca2d757e9483c0843127ec0742
polarisyb <polaris@acrosspace.com>
2023-05-28 22:40:59 +0900

    모듈화 적용 및 콘솔 출력 추가(확인용)

--- server.js
+++ server.js
@@ -4,24 +4,18 @@ const http = require('http');
 const path = require('path');
 const server = http.createServer(app);
 const io = require('socket.io')(server);
-// const { Server } = require('socket.io');
-// const io = new Server(server);
 const PORT = process.env.PORT || 8000;
 require('dotenv').config();
-
-const formatMessage = require('./utils/messages');
-const { getCurrentUser } = require('./utils/users');
 
 /* socketEvents 모듈 */
 const socketEvents = require('./socket/socketEvents');
 
 /* db 모듈 */
-// uri 유효성 검사
-const validateMongoDBUri = require('./db/UriVaildator');
+const validateMongoDBUri = require('./db/UriValidator');
 
-// 새로운 db와 컬렉션 생성
 // const createCollection = require('./db/createCollection');
 const listDatabases = require('./db/listDatabases');
+// const deleteRoomMessage = require('./db/deleteRoomMessage');
 
 // app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -49,48 +43,32 @@ const run = async (socket, client) => {
 
     await client.connect();
 
-    const db = client.db('Chat-app');
-    const coll = db.collection('message');
-
-    socket.on('chatMessage', async msg => {
-      const user = getCurrentUser(socket.id);
-
-      const message = {
-        username: user.username,
-        socketId: socket.id,
-        room: user.room,
-        message: msg,
-        time: socket.handshake.time
-      };
-
-      try {
-        await coll.insertOne(message);
-        console.log('Message inserted into MongoDB');
-
-        io.to(user.room).emit('message', formatMessage(user.username, msg, socket.handshake.time));
-
-        socketEvents.handleChatMessage(socket, io, coll);
-
-      } catch (err) {
-        console.error('Error inserting message:', err);
-      }
-    });
+    await client.db('admin').command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     await listDatabases(client);
 
   } catch (err) {
     console.error('Error:', err);
-  }
+  };
 };
 
 // connection 이 수립되면 event handler function의 인자로 socket이 들어온다.
 io.on('connection', socket => {
+  console.log(`${socket.id} is connected`);
+
   run(socket, client).catch(console.dir);
 
+  // deleteRoomMessage(client, 'JavaScript');
+  
   socket.onAny( e => {
     console.log(`Socket Event: ${e}`);
   });
 
+  const db = client.db('Chat-app');
+  const coll = db.collection('message');
+
+  socketEvents.handleChatMessage(socket, io, coll);
   socketEvents.handleJoinRoom(socket, io);
   socketEvents.handleDisconnect(socket, io);
 });

f78f8aeede2d8430c57b948b726ab87ced2288a5
polarisyb <polaris@acrosspace.com>
2023-05-29 21:14:23 +0900

    mongoDB-adapter 연습 중

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const listDatabases = require('./db/listDatabases');
 // const deleteRoomMessage = require('./db/deleteRoomMessage');
 
 // app.use(express.static('public'));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 /* mongoDB 연결 */
@@ -25,59 +26,68 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGODB_URI;
 validateMongoDBUri(uri); // MongoDB URI 유효성 검사
 
+const { createAdapter } = require("@socket.io/mongo-adapter");
+
 const client = new MongoClient(uri, {
   serverApi: {
     // ServerApiVersion.v1을 사용하여 버전 1의 서버 API를 사용하도록 지정
     version: ServerApiVersion.v1,
     // strict 옵션은 엄격한 모드를 활성화하는데 사용.
     // MongoDB 서버의 응답이 예상된 스키마와 일치하는지 확인하고, 일치하지 않으면 오류를 발생시킴
-    strict: true,
+    // strict: true,
     // deprecationErros 옵션은 mongoDB  기능이 더 이상 권장되지 않는 경우 오류를 발생시키도록 설정
     // 이를 통해 더 이상 사용되지 않는 기능을 감지하고 업데이트 할 수 있다.
     deprecationErrors: true,
+    useUnifiedTopology: true
   }
 });
 
-const run = async (socket, client) => {
+const run = async () => {
   try {
-
     await client.connect();
 
     await client.db('admin').command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
+    await client.db('adapter').createCollection('socket.io-adapter-events', {
+      capped: true,
+      size: 1e6
+    });
+
     await listDatabases(client);
+
+    const mongoCollection = client.db('adapter').collection('socket.io-adapter-events');
+    io.adapter(createAdapter(mongoCollection));
+
+    // deleteRoomMessage(client, 'JavaScript');
+
+    io.on('connection', socket => {
+      console.log(`${socket.id} is connected`);
+
+      socket.onAny(e => {
+        console.log(`Socket Event: ${e}`);
+      });
+
+      const db = client.db('Chat-app');
+      const coll = db.collection('message');
+
+      socketEvents.handleChatMessage(socket, io, coll);
+      socketEvents.handleJoinRoom(socket, io);
+      socketEvents.handleDisconnect(socket, io);
+
+      socket.on('disconnect', () => {
+        console.log(`${socket.id} is disconnected`);
+      });
+    });
 
   } catch (err) {
     console.error('Error:', err);
-  };
+  }
 };
 
-// connection 이 수립되면 event handler function의 인자로 socket이 들어온다.
-io.on('connection', socket => {
-  console.log(`${socket.id} is connected`);
+run();
 
-  run(socket, client).catch(console.dir);
-
-  // deleteRoomMessage(client, 'JavaScript');
-  
-  socket.onAny( e => {
-    console.log(`Socket Event: ${e}`);
-  });
-
-  const db = client.db('Chat-app');
-  const coll = db.collection('message');
-
-  socketEvents.handleChatMessage(socket, io, coll);
-  socketEvents.handleJoinRoom(socket, io);
-  socketEvents.handleDisconnect(socket, io);
-
-  socket.on('disconnect', () => {
-    console.log(`${socket.id} is disconnected`);
-  });
-});
-
-server.listen(PORT, err => { 
+server.listen(PORT, err => {
   if (err) throw err;
   console.log(`Server running on port ${PORT}`);
 });
